571856f4e7417ae6654a11ae26f3f678b8c3c563
hund555 <allanholmiversen@gmail.com>
2025-10-27 11:41:16 +0100

    Added nntp connection and profile selection dropdown. need unittest and show groups

--- WPF_NNTP_Project/MainWindow.xaml.cs
+++ WPF_NNTP_Project/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_NNTP_Project.Models;
 
 namespace WPF_NNTP_Project
 {
@@ -16,9 +20,70 @@ namespace WPF_NNTP_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string NntpServer = "news.sunsite.dk";
+        const int port = 119;
+        List<Profile> profiles = new List<Profile>();
         public MainWindow()
         {
             InitializeComponent();
+            profiles.Add(new Profile("Name=Select Profile"));
+            foreach (string profileLine in File.ReadAllLines("../../../Files/profiles.txt"))
+            {
+                profiles.Add(new Profile(profileLine));
+            }
+            CBProfile.ItemsSource = profiles;
+            CBProfile.SelectedIndex = 0;
+            CBProfile.DisplayMemberPath = "Name";
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            TcpClient client = new TcpClient();
+            await client.ConnectAsync(NntpServer, port);
+
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+            StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { NewLine = "\r\n", AutoFlush = true };
+
+            string? response = await reader.ReadLineAsync();
+            if (response == null || !response.Contains("200"))
+            {
+                MessageBox.Show("Failed to connect to NNTP server.");
+                return;
+            }
+            MessageBox.Show(response);
+
+
+
+            await writer.WriteLineAsync($"authinfo user {email.Text}");
+            response = await reader.ReadLineAsync();
+            if (response == null || !response.Contains("381"))
+            {
+                MessageBox.Show("Username not accepted.");
+                return;
+            }
+            MessageBox.Show(response);
+
+
+
+            await writer.WriteLineAsync($"authinfo pass {password.Password}");
+            response = await reader.ReadLineAsync();
+            if (response == null || !response.Contains("281"))
+            {
+                MessageBox.Show("Wrong password.\nAuthentication failed.");
+                return;
+            }
+            MessageBox.Show(response);
+        }
+
+        private void CBProfile_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+            {
+                Profile selectedProfile = (Profile)comboBox.SelectedItem;
+                email.Text = selectedProfile.Email;
+                password.Password = selectedProfile.Password;
+            }
         }
     }
 }
---
+++ WPF_NNTP_Project/Models/Profile.cs
@@ -0,0 +1,33 @@
+﻿namespace WPF_NNTP_Project.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Profile(string profileValues)
+        {
+            foreach(string value in profileValues.Split('|'))
+            {
+                string[] keyValue = value.Split('=');
+                switch (keyValue[0])
+                {
+                    case "Name":
+                        Name = keyValue[1];
+                        break;
+                    case "Email":
+                        Email = keyValue[1];
+                        break;
+                    case "Password":
+                        Password = keyValue[1];
+                        break;
+                }
+            }
+        }
+        public Profile()
+        {
+            
+        }
+    }
+}
